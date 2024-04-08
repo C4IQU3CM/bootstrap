560b41d309c5e3baf76562e6b156f3fd0a33f3e3
C4IQU3CM <164196966+C4IQU3CM@users.noreply.github.com>
2024-04-08 11:29:17 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+// alertas basicos
+
+/*window.alert("bem vindo") //alertas 
+window.confirm("esse é uma site teste de javascript , desculpa por qualquer erro ok?") //alerta com ok
+window.prompt("qual seu anime favorito") //alerta com pergunta
+
+// variaveis e conectação
+
+var nome = window.prompt("Qual seu nome")
+window.alert("Seja Bem Vindo "  + nome + " ao my world !")
+
+// criando 2 numeros
+
+/*var n1 = window.prompt("digite um numero")
+var n2 = window.prompt("digite mais um numero")
+var s = n1 + n2
+var s = window.alert("a soma dos 2 numeros é " + s)*/
+
+// tratamento de dados
+
+/*var n1 = Number.parseInt(window.prompt("digite um nemero"))
+var n2 = Number.parseInt(window.prompt("digite o segundo numero"))
+var s = n1 + n2
+var s = window.alert("o resultado é " + s)*/
+
+//numeros flutuantes
+
+/*var n1 = Number.parseFloat(window.prompt("digite um nemero"))
+var n2 = Number.parseFloat(window.prompt("digite o segundo numero"))
+var s = n1 + n2
+var s = window.alert("o resultado é " + s)*/
+
+//inteiros e flutuantes
+
+/*var n1 = Number(window.prompt("digite um nemero"))
+var n2 = Number(window.prompt("digite o segundo numero"))
+var s = n1 + n2
+var s = window.alert("o resultado é " + s)*/
+
+
+
+
